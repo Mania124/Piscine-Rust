78de52f2ed93e4f4ae4d3f442dfa85dedab09250
hshikuku <shikukuhezborn@gmail.com>
2025-04-02 14:54:06 +0300

    loops\: indefinite

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 
 fn main() {
-    let riddle = "Riddle: I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
+    let riddle = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
     let answer = "The letter e";
     let mut counter = 0;
     loop{
