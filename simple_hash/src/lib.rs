21f73256245aa93d35ea67a3f2ce9a118a7948ea
hshikuku <shikukuhezborn@gmail.com>
2025-04-05 18:36:42 +0300

    hashmaps

--- simple_hash/src/lib.rs
+++ simple_hash/src/lib.rs
@@ -11,37 +11,37 @@ pub fn word_frequency_counter(words: Vec<&str>) -> HashMap<&str, usize> {
 pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
     frequency_count.keys().count()
 }
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn it_works() {
-        const SENTENCE: &str = "this is a very basic sentence with only a few repetitions. once again this is very basic but it should be enough for basic tests";
-        let words = SENTENCE.split_ascii_whitespace().collect::<Vec<_>>();
-    let frequency_count = word_frequency_counter(words.clone());
-    let distict = nb_distinct_words(&frequency_count);
-    let mut expected = HashMap::new();
-        expected.insert("this", 2);
-        expected.insert("is", 2);
-        expected.insert("a", 2);
-        expected.insert("very", 2);
-        expected.insert("basic", 3);
-        expected.insert("sentence", 1);
-        expected.insert("with", 1);
-        expected.insert("only", 1);
-        expected.insert("few", 1);
-        expected.insert("repetitions.", 1);
-        expected.insert("once", 1);
-        expected.insert("again", 1);
-        expected.insert("but", 1);
-        expected.insert("it", 1);
-        expected.insert("should", 1);
-        expected.insert("be", 1);
-        expected.insert("enough", 1);
-        expected.insert("for", 1);
-        expected.insert("tests", 1);
-        assert_eq!(frequency_count,expected );
-        assert_eq!(distict, 19);
-    }
-}
+//     #[test]
+//     fn it_works() {
+//         const SENTENCE: &str = "this is a very basic sentence with only a few repetitions. once again this is very basic but it should be enough for basic tests";
+//         let words = SENTENCE.split_ascii_whitespace().collect::<Vec<_>>();
+//     let frequency_count = word_frequency_counter(words.clone());
+//     let distict = nb_distinct_words(&frequency_count);
+//     let mut expected = HashMap::new();
+//         expected.insert("this", 2);
+//         expected.insert("is", 2);
+//         expected.insert("a", 2);
+//         expected.insert("very", 2);
+//         expected.insert("basic", 3);
+//         expected.insert("sentence", 1);
+//         expected.insert("with", 1);
+//         expected.insert("only", 1);
+//         expected.insert("few", 1);
+//         expected.insert("repetitions.", 1);
+//         expected.insert("once", 1);
+//         expected.insert("again", 1);
+//         expected.insert("but", 1);
+//         expected.insert("it", 1);
+//         expected.insert("should", 1);
+//         expected.insert("be", 1);
+//         expected.insert("enough", 1);
+//         expected.insert("for", 1);
+//         expected.insert("tests", 1);
+//         assert_eq!(frequency_count,expected );
+//         assert_eq!(distict, 19);
+//     }
+// }
