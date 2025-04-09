402b873fdd37710069a2b2425b199e2e30c24d67
hshikuku <shikukuhezborn@gmail.com>
2025-04-09 10:21:03 +0300

    profanity filter

---
+++ profanity_filter/src/lib.rs
@@ -0,0 +1,29 @@
+pub struct Message {
+    pub content: String,
+    pub user: String,
+}
+
+impl Message {
+    pub fn new(content: String) -> Self {
+        Self {
+            content,
+            user: String::new(),
+        }
+    }
+
+    pub fn send_ms(&self) -> Option<String> {
+        if self.content.is_empty() || self.content.contains("stupid") {
+            None
+        } else {
+            Some(self.content.clone())
+        }
+    }
+}
+
+pub fn check_ms(message: &str) -> Result<&str, &str> {
+    let msg = Message::new(message.to_string());
+    match msg.send_ms() {
+        Some(_) => Ok(message),
+        None => Err("ERROR: illegal"),
+    }
+}
