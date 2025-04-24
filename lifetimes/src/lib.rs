869f5da8d6ba1367696aa92b2d6be4665237c682
hshikuku <shikukuhezborn@gmail.com>
2025-04-24 16:24:46 +0300

    lifetime

---
+++ lifetimes/src/lib.rs
@@ -0,0 +1,14 @@
+#[derive(Debug)]
+pub struct Person{
+	pub name: &str,
+	pub age: u8,
+}
+
+impl Person {
+	pub fn new(name: &str) -> Person {
+        Person{
+            name: name,
+            age : 0,
+        }
+	}
+}
